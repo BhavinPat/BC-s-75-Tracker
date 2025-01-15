96e31bd0661655d545e5b6625e79894425bcda1b
Bhavin Patel <bpate75@illinois.edu>
2025-01-15 10:52:05 -0600

    Initial Commit

---
+++ BC's 75 Tracker/BC_s_75_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BC_s_75_TrackerApp.swift
+//  BC's 75 Tracker
+//
+//  Created by Bhavin Patel on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BC_s_75_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BC's 75 Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BC's 75 Tracker
+//
+//  Created by Bhavin Patel on 1/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
